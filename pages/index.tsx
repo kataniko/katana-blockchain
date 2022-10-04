dff0ab02bdf3a1d70064687a2a6bddc7496c8610
Tomas Oliveira <tomasoliveira31@gmail.com>
2022-10-04 18:26:28 +0100

    ADD: main frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,79 +4,81 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-slate-900">
       <Head>
-        <title>Create Next App</title>
+        <title>Katana-Blockchain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+      ~
+      <header></header>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-slate-900">
+        <h1 className="text-6xl font-bold text-white">
+          Bem-vindo raios ta foda ao{' '}
+          <a className="text-red-600 " href="https://nextjs.org">
+            Katana-Blockchain
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
+        <p className="mt-3 text-2xl text-red-600">
+          A melhor WebApp de cripto{' '}
+          <code className="rounded-md bg-red-600 p-3 text-slate-900 font-mono text-lg">
+            katana-blockchain
           </code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://youtu.be/N2H8MRdGqzs"
+            className="mt-6 w-96 rounded-xl border border-red-600 text-red-600 p-6 text-left hover:text-white focus:text-white"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+            <h3 className="text-2xl font-bold ">niko &rarr;</h3>
+            <p className="mt-4 text-xl text-white">
+            Qual a maior causa de emigração na etiópia? 
+            O vento.
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://youtu.be/G-Wn48geCJ8"
+            className="mt-6 w-96 rounded-xl border border-red-600 text-red-600 p-6 text-left hover:text-white focus:text-white"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+            <h3 className="text-2xl font-bold">Elkidz &rarr;</h3>
+            <p className="mt-4 text-xl text-white">
+            O que faz um leproso no banho?
+            Nestum.
             </p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://youtu.be/UrbD0F91yVk"
+            className="mt-6 w-96 rounded-xl border border-red-600 p-6 text-red-600 text-left hover:text-white focus:text-white"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+            <h3 className="text-2xl font-bold ">BoneZlel &rarr;</h3>
+            <p className="mt-4 text-xl text-white">
+              BRAZILLLLLLLL
             </p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://youtu.be/CSwNHs-mgCc"
+            className="mt-6 w-96 rounded-xl border border-red-600 p-6 text-red-600 text-left hover:text-white focus:text-white"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <h3 className="text-2xl font-bold ">Katotas &rarr;</h3>
+            <p className="mt-4 text-xl text-white">
+              Bruhh
             </p>
           </a>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      <footer className="flex h-24 w-full items-center justify-center border-t bg-slate-900">
         <a
-          className="flex items-center justify-center gap-2"
+          className="flex items-center justify-center gap-2 text-red-600"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          feito por{' '}
+          <Image src="/unknown.svg" alt="Katana logo" width={72} height={16} />
         </a>
       </footer>
     </div>
