ec1e4e4e702f1ed5bf17c9069c9be9d3168db892
Tomas Oliveira <tomasoliveira31@gmail.com>
2022-10-27 20:30:54 +0100

    ADD: const frontend

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   Images: {
-    domains: ['s2.coinmarketcap.com','s3.coinmarketcap.com', 
-    ' encrypted-tbn0.gstatic.com']
+    domains: ['s2.coinmarketcap.com']
   },
 }
