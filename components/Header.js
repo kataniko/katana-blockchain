7c756112104b912ca332f2f320c75d7f1d79500f
kataniko <tomasoliveira31@gmail.com>
2022-10-28 18:11:51 +0100

    ADD: Header frontend

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
                     <div className={styles.navItem}>
                         <div className={styles.navLink}>
                             <div className='antialiased hover:subpixel-antialiased font-serif  '>
-                             Assets
+                             Assetsss
                             </div>
                             </div>    
                         <div className={styles.badge} />
